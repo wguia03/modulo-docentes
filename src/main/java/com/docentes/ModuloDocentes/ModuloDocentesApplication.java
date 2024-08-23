e5d36c952b8ed889c02a425b1e91f3ec35f18abe
wguia03 <126293468+wguia03@users.noreply.github.com>
2024-08-23 18:33:08 -0500

    first commit

---
+++ src/main/java/com/docentes/ModuloDocentes/ModuloDocentesApplication.java
@@ -0,0 +1,13 @@
+package com.docentes.ModuloDocentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloDocentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloDocentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docentes/ModuloDocentes/ModuloDocentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docentes.ModuloDocentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloDocentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
